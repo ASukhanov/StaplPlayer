5ab8267608d410a8f2e0f358eb7d21bbc2ec8384
ASukkhanov <sukhanov@bnl.gov>
2014-08-22 18:00:04 -0400

    stapl_interactive.py: handles several commands per line, comments, direct commands

--- stapl_interactive.py
+++ stapl_interactive.py
@@ -1,57 +1,70 @@
 # Interactive wrapper for StaplPlayer
-# Version 2	2014-08-21
-# Example of a session of shifting in 3 numbers into IR$10 and IR$12 :
-#i10
-#1
-#2
-#3
-#x	#execute the list
-#x	#repeat the list
-#n	#reset the list, start new one
-#i12
-#1
-#2
-#3
-#x
+# Version 3	2014-08-22
+#
+# Example of a session:
+#i10 20 30 40 #comment1
+#'this line will be added for execution
+#40 50 #comment2 60 70
 
 import subprocess
+import string
 
-irscan_lines = ''
-drscan_lines = ''
-
+outLines = ''
+print('Enter list of commands finished by empty line:')
 while 1:
-  s=raw_input('')
-  if len(s) == 0:
-      exit()
-  elif s[0] == 'x':
-    f = open('stapl.stp','w')
+  l=raw_input('')
+  if len(l) == 0:
+     continue
+  if l[0] == 'p':	# build the stapl file /run/shm/stapl.stp and play it
+    f = open('/run/shm/stapl.stp','w')
     f.write('ACTION	TRANS = DO_TRANS;\n')
     f.write('DATA PARAMETERS;\n')
     f.write('BOOLEAN idcode_data[32*10];\n')
     f.write('BOOLEAN irdata[10*32];\n')
+    f.write('INTEGER ii;\n')
     f.write('ENDDATA;\n')
-
+    
     f.write('PROCEDURE DO_TRANS USES PARAMETERS;\n')
-    f.write(irscan_lines)
-    f.write(drscan_lines)
+    f.write(outLines)
     f.write('ENDPROC;\n')
     f.close()
     
     #print("Stapl instructions:\n")
-    #f = open('stapl.stp','r')
+    #f = open('/run/shm/stapl.stp','r')
     #for s in f:
     #  print(s),
-    #f.close()
+    #  f.close()
+    
     # execute action
-    p = subprocess.Popen('StaplPlayer -aTRANS stapl.stp', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    line = 'StaplPlayer -aTRANS /run/shm/stapl.stp'
+    print('Executing "'+line+'"');
+    p = subprocess.Popen(line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
-      print line,
+      if line[0:6] == 'Export':
+        print(line.split('HEX ')[1]),
+      #print line,
       retval = p.wait()
-  elif s[0] == 'i':
-    irscan_lines += 'IRSCAN 8, $' + s[1:] + ', CAPTURE irdata[7..0];\n'
-  elif s[0] == 'n':	#reset command list
-    irscan_lines = ''
-    drscan_lines = ''
-  else:  
-    drscan_lines += 'DRSCAN 32, $' + s + ', CAPTURE idcode_data[31..0];\n'
-    drscan_lines += 'EXPORT "Shifted Out:", idcode_data[31..0];\n'
+    continue
+
+  if l[0] == 'x':
+    exit()
+  if l[0] == 'n':   # reset command list
+    print('List reset')
+    outLines = ''
+  if l[0] == '\'':
+    outLines += l[1:] + '\n'
+    continue
+
+  for s in l.split():
+    #s.lstrip('>')
+    if s[0] == '#':	# comment
+      break
+    elif s[0] == 'i':	# IRSCAN instruction
+      outLines += 'IRSCAN 8, $' + s[1:] + ', CAPTURE irdata[7..0];\n'
+    elif s[0] == 'n':	# reset command list
+      outLines = ''
+    elif all(c in string.hexdigits for c in s):	# DRSCAN instruction
+      outLines += 'DRSCAN 32, $' + s + ', CAPTURE idcode_data[31..0];\n'
+      outLines += 'EXPORT "Shifted Out:", idcode_data[31..0];\n'
+    else:
+      print('Unknown token: '+s)
