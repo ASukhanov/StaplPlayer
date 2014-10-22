1211cf0d0722dcc3a00dce1207d5ec3bcf1728a8
ASukhanov <sukhanov@bnl.gov>
2014-10-22 08:48:33 -0400

    option -c in stapl_cmdline.py

--- stapl_cmdline.py
+++ stapl_cmdline.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # Wrapper for StaplPlayer
 # Packs program arguments into a STAPL file stapl.stp and plays it using StaplPlayer
-# Version 2	2014-08-21
+# Version 3	2014-10-21
+
 import sys
+
+#the following is needed only for -c option
+import wiringpi2
+
 if len(sys.argv) < 2:
   print('No arguments\n Usage example : staplcmd i8 1 2 i10 deadface fe0dfe0f')
   print('will do:\nIRSCAN $8; DRSCAN 32 $1; DRSCAN 32 $2;')
@@ -18,10 +23,16 @@ f.write('ENDDATA;\n')
 
 f.write('PROCEDURE DO_TRANS USES PARAMETERS;\n')
 splayer_option = ''
+work_with_carrier = 0
 
 for s in sys.argv[1:]:
   if s == '-g': #use second JTAG chain
     splayer_option = '-g'
+    continue
+  if s == '-c': # set the path to work with carrier boards
+    import wiringpi2
+    wiringpi2.wiringPiSetupSys()
+    work_with_carrier = 8	# GPIO for the carrier select[0] line in RPiLVDS board
     continue
   if s[0] == 'i':
     f.write('IRSCAN 8, $' + s[1:] + ', CAPTURE irdata[7..0];\n')
@@ -30,6 +41,12 @@ for s in sys.argv[1:]:
     f.write('EXPORT "Shifted Out:", idcode_data[31..0];\n')
 f.write('ENDPROC;\n')
 f.close()
+
+if (work_with_carrier != 0):	# set route to carrier board
+  if (splayer_option == '-g'):
+     work_with_carrier = 7	# GPIO for the carrier select[1] line in RPiLVDS board
+  wiringpi2.digitalWrite(work_with_carrier,1)
+
 
 #print("Stapl instructions:\n")
 #f = open('stapl.stp','r')
@@ -46,4 +63,7 @@ p = subprocess.Popen(cmdline, shell=True, stdout=subprocess.PIPE, stderr=subproc
 for line in p.stdout.readlines():
   print line,
 retval = p.wait()
+
+if (work_with_carrier != 0):
+  wiringpi2.digitalWrite(work_with_carrier,0)
 
