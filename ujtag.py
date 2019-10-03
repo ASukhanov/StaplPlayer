0e4225b711326d6bfcda2a3ed56f30a69f47679c
Andrei Sukhanov <sukhanov@bnl.gov>
2019-10-03 13:40:23 -0400

    cleanup

--- ujtag.py
+++ ujtag.py
@@ -1,87 +1,88 @@
 #!/usr/bin/env python3
-'''Slow control through JTAG connector'''
+'''Slow Control through JTAG connector'''
 
-#__version__ = 'v02 2019-09-19'#created
-__version__ = 'v03 2019-09-20'# gpiozero lib is too slow
+__version__ = 'v07 2019-09-25'#docstrings
 
 import sys,time
 from timeit import default_timer as timer
-#from gpiozero import LEDBoard,Button
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except Exception as e:
+    print('WARNING:'+str(e))
 TCK,TMS,TDI,TDO = 4,17,18,27
 
 class UJTAG():
+    '''Interface to FPGA fabric using JTAG connector'''
     def __init__(self,dbg=False):
+        '''Configure GPIO channels'''
         global Dbg
         Dbg = dbg
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(TCK, GPIO.OUT)
-        GPIO.setup(TMS, GPIO.OUT)
-        GPIO.setup(TDI, GPIO.OUT)
-        GPIO.setup(TDO, GPIO.IN)
+        try:
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(TCK, GPIO.OUT)
+            GPIO.setup(TMS, GPIO.OUT)
+            GPIO.setup(TDI, GPIO.OUT)
+            GPIO.setup(TDO, GPIO.IN)
+        except Exception as e:
+            print('WARNING:'+str(e))
         self.cmi = (TCK,TMS,TDI)
         self.lastTDI = None
         self.lastTMS = None
+        self.lastIR = None
                 
     def __del__(self):
-        #self.spi.close()
+        '''Cleanup, return all used channels to inputs with no pull up/down'''
+        #GPIO.cleanup()
         pass
         
     def _tdo(self):
-        #return int(self.pinTDO.is_pressed)
+        # Return state of the TDO line
         return(GPIO.input(TDO))
             
     def board_info(self):
-        from gpiozero import pi_info
-        print('{0}'.format(pi_info()))
-        #print('{0:full}'.format(pi_info()))
-        #print('{0:board}'.format(pi_info()))
-        #print('{0:specs}'.format(pi_info()))
-        #print('{0:headers}'.format(pi_info()))
+        print('UJTAG host:'+str(GPIO.RPI_INFO))
     
     def _tms_tdi(self,tms,tdi):
         #print('tms%i,tdi%i'%(tms,tdi))
-        #GPIO.output(TMS,tms)
-        #GPIO.output(TDI,tdi)
         GPIO.output(self.cmi,(0,tms,tdi))
         GPIO.output(TCK,1)
-        #GPIO.output(self.cmi,(1,tms,tdi))
         GPIO.output(TCK,0)
         return GPIO.input(TDO)
         
     def reset(self,go2run=False):
-        '''go to Idle or Run state through Test-Logic-Reset from any state'''
+        '''Go to Idle or Run state through Test-Logic-Reset from any state'''
         for i in range(5):
             self._tms_tdi(1,0)
         self._tms_tdi(0,0)
         if go2run:
             self._tms_tdi(1,0)
         
-    def irscan(self,data,width=8):
-        '''do irscan starting from Run finish in Run state'''
-        #self._tms_tdi(0,0)
-        #self._tms_tdi(1,0)
+    def irscan(self,ir,width=8):
+        '''Do irscan starting from Run state finish in Run state'''
+        if ir == self.lastIR:
+            return
+        self.lastIR = ir
         self._tms_tdi(1,0)
         self._tms_tdi(0,0)#>capture
         self._tms_tdi(0,0)#>shift-ir
         #print('>irshift')
         for i in range(width-1): #shift
-            #self._tms_tdi(0,(data>>(width-i-1))&1)
-            self._tms_tdi(0,data&1)
-            data = data >> 1
-        self._tms_tdi(1,data&1)#>exit-ir
+            self._tms_tdi(0,ir&1)
+            ir = ir >> 1
+        self._tms_tdi(1,ir&1)#>exit-ir
         self._tms_tdi(1,0)#>update-ir
         self._tms_tdi(1,0)#>Run
     
-    def drscan(self,data,width=32,go2idle=False):
-        '''do drscan starting from Run finish in Run state'''
+    def drscan(self,data,ir=None,width=32,go2idle=False):
+        '''Do drscan starting from Run state finish in Run state'''
+        if ir is not None:
+            self.irscan(ir)
         self._tms_tdi(0,0)#>capture
         self._tms_tdi(0,0)#>shift-dr
         d = 0
         #print('>drshift')
         for i in range(width-1): #shift
-            #tdo = self._tms_tdi(0,(data>>(width-i-1))&1)
             tdo = self._tms_tdi(0,data&1)
             data = data >> 1
             d |= tdo << i+1
@@ -90,7 +91,58 @@ class UJTAG():
         self._tms_tdi(1,0)#>update-dr
         self._tms_tdi(0 if go2idle else 1, 0)#>Idle or Run
         return d
+uj = UJTAG()
+
+class CSR():
+    '''UJTAG-compatible Command and Status Register''' 
+    def __init__(self,ir, d={}, q={}, name='?'):
+        '''Create the CSR
+        d: specifies the input bit fields
+        q: specifies the output bit fields
+        '''
+        self.ir = ir
+        self.name = name
+        self.lastQ = 0
+        self.dFields = {}# map of input fields
+        self.qFields = {}# map of output fields
         
+        def _set_fields(items,fields):
+            for key,bitRange in items:
+                startBit,endBit = bitRange
+                mask = 0
+                for i in range(startBit,endBit+1):
+                    mask |= 1<<i
+                fields[key] = startBit,mask
+        _set_fields(q.items(),self.qFields)
+        _set_fields(d.items(),self.dFields)        
+            
+    def update(self,q=0):
+        '''Read current value, set new value to q, current value is returned'''
+        d = uj.drscan(q,self.ir)
+        self.lastQ = q
+        return d
+
+    def fields(self,dq='d'):
+        '''Return list of BitFields'''
+        return self.qFields.keys() if dq=='q' else self.dFields.keys()
+        
+    def get_field(self,name):
+        '''Return value of the named field'''
+        #print('CSR getter '+str(name))
+        d = self.update()
+        #print('BitField Getter called: %s='%self.name+hex(d))
+        startBit,mask = self.dFields[name]
+        d = (d & mask) >> startBit
+        return d
+        
+    def set_field(self,name,value):
+        '''Set new value to the named field'''
+        startBit,mask = self.qFields[name]
+        qClearField = self.lastQ & ~mask
+        q = qClearField | ((value << startBit)&mask)
+        d = self.update(q)
+        print('Setter called: %s=%s from %s, '%(name,hex(q),hex(d)))
+        return d        
 #````````````````````````````Test program`````````````````````````````````````
 if __name__ == "__main__":
     import argparse
@@ -120,24 +172,22 @@ if __name__ == "__main__":
 
     ts = timer()
     for i in range(pargs.n):
+        uj.irscan(0x10)
+        ir10d = uj.drscan(0)
+        print('IR10:'+hex(ir10d))
+
+        uj.irscan(0x90)
+        ir90d = uj.drscan(0)
+        print('IR10_Shadow:'+hex(ir90d))
+        if ir10d != ir90d:
+            print('WARNING: Shadow register IR90='+hex(ir90d)\
+            +'expected:'+hex(ir10d))
+        
         uj.irscan(0x1d)
         d = 0xc00c7007
         q = uj.drscan(d)
         print('captured:'+hex(q)+', written:'+hex(d))
         q = uj.drscan(d)
         print('captured:'+hex(q)+', written:'+hex(d))
-        
-        #the shadow register 9d should show the same but it shows 0
-        #uj.irscan(0x9d)
-        #q = uj.drscan(0)
-        #print('captured from 9d:'+hex(q))        
-
-        #board reset
-        #uj.irscan(0x10)
-        #uj.drscan(0x1000)
-        #uj.drscan(0x0000)
-
-        #print('cycle %i, '%i + str(timer()-ts))
-        #time.sleep(1)
     print('cycles %i, '%pargs.n + str(timer()-ts))
         
